1a81992d268edf9192ed19342f470d9352a94cb7
rajiv <rajiv.mucheli@gmail.com>
2025-05-21 11:34:51 +0530

    Update kmip_routes.py

--- pykmip-restapi/routes/kmip_routes.py
+++ pykmip-restapi/routes/kmip_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from services.kmip_service import KMIPService
+import logging
 
 class KMIPRoutes:
     def __init__(self, kmip_service):
@@ -59,7 +60,6 @@ class KMIPRoutes:
             }), 200
 
         except Exception as e:
-            import logging
             logging.error("An unexpected error occurred", exc_info=True)
             return jsonify({"error": "An internal error has occurred. Please contact support."}), 500
 
@@ -122,6 +122,5 @@ class KMIPRoutes:
             }), 200
 
         except Exception as e:
-            import logging
             logging.error("An unexpected error occurred", exc_info=True)
             return jsonify({"error": "An internal error has occurred. Please contact support."}), 500
